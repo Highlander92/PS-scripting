be9a5187edcb82360f387117974f4afbaa70ef71
Highlander92 <jonathanm28@hotmail.com>
2016-06-21 20:22:57 +0000

    Add files via upload
    work in progress, having issues displaying some of the dummy IP addresses. I want to have all that sorted out and retrieved correctly prior to expanding more on the structure

---
+++ structure.ps1
@@ -0,0 +1,20 @@
+﻿#calls JSON file
+$json = (Get-Content C:\catalog.json -Raw)
+$json| ConvertFrom-Json | select -ExpandProperty vnx 
+
+#variable ****
+$devices = $ | 
+#gets the properities and methods 
+Get-Member -MemberType Properties | 
+Select-Object -ExpandProperty 
+
+#***** fiddling with 
+ForEach($devices in $devices) {
+    If($ ){
+       Write-Output "$devices "
+    }
+}
+
+
+
+
