8f9b1dc3574e8c7aa6264667e9a4a673ad7c1bb1
Highlander92 <jonathanm28@hotmail.com>
2016-06-28 14:48:18 -0400

    Add files via upload

--- structure.ps1
+++ structure.ps1
@@ -1,57 +1,63 @@
-﻿#calls JSON file
+﻿<#calls JSON file
 $json = (Get-Content C:\catalog.json -Raw)
-$json| ConvertFrom-Json | select -ExpandProperty vnx 
+$json| ConvertFrom-Json #| select -ExpandProperty vnx 
+#>
 
-<#
-$devices = $ | 
+
+<#Not needed for now/Possibly later on
+$devices = 
 #gets the properities and methods 
 Get-Member -MemberType Properties | 
 Select-Object -ExpandProperty 
 #>
 
+#variables
 $VNXSerial1 = "192.168.2.204" 
 $VNXSerial2 = "192.168.2.206"
 $VNXSerial3 = "192.168.2.207"
 $VNXSerial4 = "192.168.2.205"
 
+#Loop
 $VNX = "$VNXSerial1 , $VNXSerial2 , $VNXSerial3, $VNXSerial4" 
 foreach ($device in $VNX) 
-{if ($VNX -eq 'condition') { break }
+{if ($device -eq 'condition') { break }
 }
-#{ "$device = " + $device.'insert' }
 
 
+#Variables
 $VPLEXSerial1 ="192.168.2.223"
 $VPLEXSerial2 = "192.168.2.245"
 $VPLEXSerial3 = "192.168.2.223"
 $VPLEXSerial4 = "192.168.2.265"
 
+#loop
 $VPLEX = "$VPLEXSerial1, $VPLEXSerial2, $VPLEXSerial3, $VPLEXSerial4"
 foreach ($device in $VPLEX) 
-{if ($VPLEX -eq 'condition') { break }
+{if ($device -eq 'condition') { break }
 }
-#{ "$device = " + $device.'insert' }
 
 
 
+#variables 
 $CenteraSerial1 = "10.236.2.6:3682"
 $CenteraSerial2 = "10.236.1.6:3682"
 $CenteraSerial3 = "10.236.1.6:3683"
 $CenteraSerial4 = "10.236.1.6:3685"
 
+#loop
 $Centera = "$CenteraSerial1, $CenteraSerial2, $CenteraSerial3, $CenteraSerial4"
 foreach ($device in $Centera) 
-{if ($Centera -eq 'condition') { break }
+{if ($device -eq 'condition') { break }
 }
-#{ "$device = " + $device.'insert' }
 
+#variables
 $VmaxSerial1 = "192.168.2.37"
 $VmaxSerial2 = "192.168.2.238"
 $VmaxSerial3 = "192.168.2.239"
 $VmaxSerial4 = "192.168.2.236"
 
+#loop
 $Vmax = "$VmaxSerial1, $VmaxSerial2, $VmaxSerial3, $VmaxSerial4"
 foreach ($device in $Vmax) 
-{if ($Vmax -eq 'condition') { break }
+{if ($device -eq 'condition') { break }
 }
-#{ "$device = " + $device.'insert' }
